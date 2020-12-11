d288721edc44804f755814fc96d588ba33bbaa5b
Kanstantsin Yakimovich <yakimovichkostya@gmail.com>
2020-12-11 14:34:46 +0300

    feat: task solution

---
+++ index.js
@@ -0,0 +1,86 @@
+const data = {
+    "491":
+    {"manager_id":"491",
+    "data":{"2019-3":
+    {"date":"2019-3","plan_value":10000,"C3:WON":14600,"C4:WON":null,"C5:WON":null,"total_value":14600,"nadbavka":292},
+    "2019-4":{"date":"2019-4","plan_value":10000,"C3:WON":6500,"C4:WON":null,"C5:WON":null,"total_value":6500,"nadbavka":0},
+    "2019-7":{"date":"2019-7","plan_value":13000,"C3:WON":10550,"C4:WON":null,"C5:WON":null,"total_value":10550,"nadbavka":0},
+    "2019-6":{"date":"2019-6","plan_value":10000,"C3:WON":14610,"C4:WON":null,"C5:WON":null,"total_value":14610,"nadbavka":292.2},
+    "2019-5":{"date":"2019-5","plan_value":10000,"C3:WON":9250,"C4:WON":null,"C5:WON":null,"total_value":9250,"nadbavka":0},
+    "2019-8":{"date":"2019-8","plan_value":16000,"C3:WON":16850,"C4:WON":null,"C5:WON":null,"total_value":16850,"nadbavka":337},
+    "2019-9":{"date":"2019-9","plan_value":13575,"C3:WON":13600,"C4:WON":null,"C5:WON":null,"total_value":13600,"nadbavka":272},
+    "2019-10":{"date":"2019-10","plan_value":15694,"C3:WON":18200,"C4:WON":null,"C5:WON":null,"total_value":18200,"nadbavka":364},
+    "2019-11":{"date":"2019-11","plan_value":19000,"C3:WON":21030,"C4:WON":null,"C5:WON":null,"total_value":21030,"nadbavka":420.6},
+    "2019-12":{"date":"2019-12","plan_value":19000,"C3:WON":24270,"C4:WON":null,"C5:WON":null,"total_value":24270,"nadbavka":485.40000000000003},
+    "2020-1":{"date":"2020-1","plan_value":19000,"C3:WON":18630,"C4:WON":null,"C5:WON":null,"total_value":18630,"nadbavka":0},
+    "2020-2":{"date":"2020-2","plan_value":19000,"C3:WON":17060,"C4:WON":null,"C5:WON":null,"total_value":17060,"nadbavka":0},
+    "2020-3":{"date":"2020-3","plan_value":19000,"C3:WON":21830,"C4:WON":null,"C5:WON":null,"total_value":21830,"nadbavka":436.6},
+    "2020-4":{"date":"2020-4","plan_value":11448,"C3:WON":11560,"C4:WON":null,"C5:WON":null,"total_value":11560,"nadbavka":231.20000000000002},
+    "2020-5":{"date":"2020-5","plan_value":21000,"C3:WON":21640,"C4:WON":null,"C5:WON":null,"total_value":21640,"nadbavka":432.8},
+    "2020-6":{"date":"2020-6","plan_value":21000,"C3:WON":29900,"C4:WON":null,"C5:WON":null,"total_value":29900,"nadbavka":598},
+    "2020-7":{"date":"2020-7","plan_value":21000,"C3:WON":22200,"C4:WON":null,"C5:WON":null,"total_value":22200,"nadbavka":444},
+    "2020-9":{"date":"2020-9","plan_value":21000,"C3:WON":12700,"C4:WON":null,"C5:WON":null,"total_value":12700,"nadbavka":0},
+    "2020-8":{"date":"2020-8","plan_value":11000,"C3:WON":7000,"C4:WON":null,"C5:WON":null,"total_value":7000,"nadbavka":0},
+    "2020-10":{"date":"2020-10","plan_value":21000,"C3:WON":21000,"C4:WON":null,"C5:WON":null,"total_value":21000,"nadbavka":420},
+    "2020-11":{"date":"2020-11","plan_value":21000,"C3:WON":22380,"C4:WON":null,"C5:WON":null,"total_value":22380,"nadbavka":447.6},
+    "2020-12":{"date":"2020-12","plan_value":21000,"C3:WON":4100,"C4:WON":null,"C5:WON":null,"total_value":4100,"nadbavka":0},
+    "1970-1":{"date":"1970-1","plan_value":0,"C3:WON":null,"C4:WON":null,"C5:WON":null,"total_value":0,"nadbavka":0}},"plan_value":0},
+    "509":
+    {"manager_id":"509",
+    "data":{"2019-4":
+    {"date":
+    "2019-4","plan_value":10000,"C3:WON":9150,"C4:WON":null,"C5:WON":null,"total_value":9150,"nadbavka":0},
+    "2019-3":{"date":"2019-3","plan_value":10000,"C3:WON":10500,"C4:WON":null,"C5:WON":null,"total_value":10500,"nadbavka":210},
+    "2019-7":{"date":"2019-7","plan_value":13000,"C3:WON":10800,"C4:WON":null,"C5:WON":null,"total_value":10800,"nadbavka":0},
+    "2019-6":{"date":"2019-6","plan_value":10000,"C3:WON":16300,"C4:WON":null,"C5:WON":null,"total_value":16300,"nadbavka":326},
+    "2019-5":{"date":"2019-5","plan_value":10000,"C3:WON":16240,"C4:WON":null,"C5:WON":null,"total_value":16240,"nadbavka":324.8},
+    "2019-9":{"date":"2019-9","plan_value":0,"C3:WON":27650,"C4:WON":3000,"C5:WON":null,"total_value":30650,"nadbavka":0},
+    "2019-8":{"date":"2019-8","plan_value":16000,"C3:WON":23300,"C4:WON":null,"C5:WON":null,"total_value":23300,"nadbavka":466},
+    "2019-10":{"date":"2019-10","plan_value":19000,"C3:WON":22300,"C4:WON":null,"C5:WON":null,"total_value":22300,"nadbavka":446},
+    "2019-11":{"date":"2019-11","plan_value":19000,"C3:WON":10750,"C4:WON":null,"C5:WON":null,"total_value":10750,"nadbavka":0},
+    "2019-12":{"date":"2019-12","plan_value":19000,"C3:WON":26650,"C4:WON":null,"C5:WON":null,"total_value":26650,"nadbavka":533},
+    "2020-1":{"date":"2020-1","plan_value":19000,"C3:WON":21080,"C4:WON":null,"C5:WON":null,"total_value":21080,"nadbavka":421.6},
+    "2020-2":{"date":"2020-2","plan_value":0,"C3:WON":21720,"C4:WON":null,"C5:WON":null,"total_value":21720,"nadbavka":0},
+    "2020-3":{"date":"2020-3","plan_value":10356,"C3:WON":11900,"C4:WON":null,"C5:WON":null,"total_value":11900,"nadbavka":238},
+    "2020-4":{"date":"2020-4","plan_value":21000,"C3:WON":29130,"C4:WON":null,"C5:WON":null,"total_value":29130,"nadbavka":582.6},
+    "2020-5":{"date":"2020-5","plan_value":21000,"C3:WON":22010,"C4:WON":null,"C5:WON":null,"total_value":22010,"nadbavka":440.2},
+    "2020-6":{"date":"2020-6","plan_value":21000,"C3:WON":21170,"C4:WON":null,"C5:WON":null,"total_value":21170,"nadbavka":423.40000000000003},
+    "2020-7":{"date":"2020-7","plan_value":21000,"C3:WON":25000,"C4:WON":null,"C5:WON":null,"total_value":25000,"nadbavka":500},
+    "2020-8":{"date":"2020-8","plan_value":21000,"C3:WON":31000,"C4:WON":null,"C5:WON":null,"total_value":31000,"nadbavka":620},
+    "2020-9":{"date":"2020-9","plan_value":21000,"C3:WON":9050,"C4:WON":null,"C5:WON":null,"total_value":9050,"nadbavka":0},
+    "2020-11":{"date":"2020-11","plan_value":21000,"C3:WON":22660,"C4:WON":null,"C5:WON":1700,"total_value":24360,"nadbavka":487.2},
+    "2020-10":{"date":"2020-10","plan_value":11448,"C3:WON":14190,"C4:WON":null,"C5:WON":null,"total_value":14190,"nadbavka":283.8},
+    "2020-12":{"date":"2020-12","plan_value":21000,"C3:WON":9450,"C4:WON":null,"C5:WON":null,"total_value":9450,"nadbavka":0},
+    "1970-1":{"date":"1970-1","plan_value":0,"C3:WON":null,"C4:WON":null,"C5:WON":null,"total_value":0,"nadbavka":0}},"plan_value":0},
+    "731":
+    {"manager_id":"731",
+    "data":{"2019-9":{"date":
+      "2019-9","plan_value":0,"C3:WON":null,"C4:WON":null,"C5:WON":null,"total_value":0,"nadbavka":0},
+      "2019-10":{"date":"2019-10","plan_value":19000,"C3:WON":19000,"C4:WON":null,"C5:WON":null,"total_value":19000,"nadbavka":380},
+      "2019-11":{"date":"2019-11","plan_value":19000,"C3:WON":19200,"C4:WON":null,"C5:WON":null,"total_value":19200,"nadbavka":384},
+      "2019-12":{"date":"2019-12","plan_value":19000,"C3:WON":15800,"C4:WON":null,"C5:WON":null,"total_value":15800,"nadbavka":0},
+      "2020-1":{"date":"2020-1","plan_value":19000,"C3:WON":15350,"C4:WON":null,"C5:WON":null,"total_value":15350,"nadbavka":0},
+      "2020-2":{"date":"2020-2","plan_value":19000,"C3:WON":19370,"C4:WON":null,"C5:WON":null,"total_value":19370,"nadbavka":387.40000000000003},
+      "2020-3":{"date":"2020-3","plan_value":19000,"C3:WON":19300,"C4:WON":null,"C5:WON":null,"total_value":19300,"nadbavka":386},
+      "2020-4":{"date":"2020-4","plan_value":21000,"C3:WON":23520,"C4:WON":null,"C5:WON":null,"total_value":23520,"nadbavka":470.40000000000003},
+      "2020-5":{"date":"2020-5","plan_value":21000,"C3:WON":24390,"C4:WON":null,"C5:WON":null,"total_value":24390,"nadbavka":487.8},
+      "2020-6":{"date":"2020-6","plan_value":21000,"C3:WON":24950,"C4:WON":null,"C5:WON":null,"total_value":24950,"nadbavka":499},
+      "2020-7":{"date":"2020-7","plan_value":21000,"C3:WON":25950,"C4:WON":null,"C5:WON":null,"total_value":25950,"nadbavka":519},
+      "2020-8":{"date":"2020-8","plan_value":21000,"C3:WON":26500,"C4:WON":null,"C5:WON":null,"total_value":26500,"nadbavka":530},
+      "2020-9":{"date":"2020-9","plan_value":10494,"C3:WON":4450,"C4:WON":null,"C5:WON":null,"total_value":4450,"nadbavka":0},
+      "2020-10":{"date":"2020-10","plan_value":21000,"C3:WON":23190,"C4:WON":null,"C5:WON":null,"total_value":23190,"nadbavka":463.8},
+      "2020-11":{"date":"2020-11","plan_value":21000,"C3:WON":25260,"C4:WON":null,"C5:WON":null,"total_value":25260,"nadbavka":505.2},
+      "2020-12":{"date":"2020-12","plan_value":21000,"C3:WON":4850,"C4:WON":null,"C5:WON":null,"total_value":4850,"nadbavka":0},
+      "1970-1":{"date":"1970-1","plan_value":0,"C3:WON":null,"C4:WON":null,"C5:WON":null,"total_value":0,"nadbavka":0}},"plan_value":0}}
+    const managerDate = "2020-5";
+    
+    const arr = Object.entries(data);
+    const helpArr =[];
+    const result = arr.forEach(item=>{
+    const obj = item[1].data; 
+   for(key in obj){    
+        if(key===managerDate) helpArr.push(obj[key].plan_value);         
+    }
+   });
+    const res = helpArr.reduce((acc,el)=>acc+=el,0);
+    console.log(res);
